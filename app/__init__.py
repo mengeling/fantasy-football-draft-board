1749498721faca1776b4a482d40c91cc27194ac0
Michael Engeling <michael.engeling@gmail.com>
2019-05-21 22:01:15 -0500

    Updated draft board table to include everything

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
 from sqlalchemy import create_engine
 
@@ -7,21 +7,35 @@ import constants as c
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/", methods=["GET"])
 def index():
     """
-    Renders index.html template for the main page of the app, using draft board
+    Renders index.html template with draft board table and selected/top player highlighted
     """
-    return render_template('index.html', data=data)
+
+    # Retrieve player ID
+    id = request.args.get("id", None)
+    if id:
+        df_player = df[df["id"] == id]
+    else:
+        df_player = df.iloc[df["rank"].idxmin()]
+        id = df_player["id"]
+
+    # Use ID to retrieve image path along with player bio, rankings, and stats
+    img_path = "img/{}.jpg".format(id)
+    board = df_board.to_html(index=False, escape=False)
+    return render_template("index.html", board=board, img_path=img_path)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     # Create DB engine and read draft board table into pandas df
     engine = create_engine(c.DB_ENGINE)
     df = pd.read_sql_query("SELECT * FROM draft_board", con=engine)
-    df.columns = df.columns.str.upper()
-    data = df.to_html(index=False, escape=False)
+
+    # Create separate draft board df for the HTML table and update headers
+    df_board = df[c.DRAFT_BOARD_HEADERS].copy()
+    df_board.columns = c.DRAFT_BOARD_DISPLAYED_HEADERS
 
     # Run the app
     app.run(debug=True)
--- app/constants.py
+++ app/constants.py
@@ -1,3 +1,51 @@
 IMG_PATH = "../app/static/img/"
 
 DB_ENGINE = "postgresql://mengeling:mengeling@localhost:5432/ffball"
+
+DRAFT_BOARD_HEADERS = [
+    "rank",
+    "player",
+    "bye_week",
+    "position_ranking",
+    "best_ranking",
+    "worst_ranking",
+    "avg_ranking",
+    "std_dev_ranking",
+    "avg_draft_pick",
+    "fantasy_pts",
+    "pass_cmp",
+    "pass_att",
+    "pass_yds",
+    "pass_td",
+    "pass_int",
+    "rush_att",
+    "rush_yds",
+    "rush_td",
+    "receptions",
+    "rec_yds",
+    "rec_td"
+]
+
+DRAFT_BOARD_DISPLAYED_HEADERS = [
+    "RANK",
+    "PLAYER",
+    "BYE",
+    "POS",
+    "BEST",
+    "WORST",
+    "AVG",
+    "STDEV",
+    "ADP",
+    "PTS",
+    "COM",
+    "ATT",
+    "PAYD",
+    "PATD",
+    "PAINT",
+    "RUSH",
+    "RUYD",
+    "RUTD",
+    "REC",
+    "REYD",
+    "RETD"
+]
